e59e8cd7a95c7f83525fc5ba2901508330a14462
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-01-22 21:40:27 +0600

    first

--- assignment.js
+++ assignment.js
@@ -19,8 +19,28 @@ result2 = budgetCalculator(4,5,6);
 console.log(result2);
 
 function hotelCost(days){
-    
+    var price = 0;
+    if(days <= 10){
+        price = days*100;
+    }else if(days <= 20){
+        var firstP  = 10*100;
+        var remainP = days - 10;
+        var secondP = remainP * 80;
+        price = firstP + secondP; 
+    }else{
+        var firstP  = 10 * 100;
+        var secondP = 10 * 80;
+        var remainP = days - 20;
+        var thirdP  = remainP * 50;
+        price       = (firstP + secondP + thirdP); 
+    }
+    return price;
+
 }
+result3 = hotelCost(30);
+console.log(result3);
+
+
 
 
 
