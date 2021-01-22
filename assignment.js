b11e36697e029425b32ae3c79e81265ea6642f3a
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-01-22 21:08:27 +0600

    first

---
+++ assignment.js
@@ -0,0 +1,28 @@
+function kilometerToMeter(value) {
+    //1Km = 1000m
+    var meter = value*1000;
+    
+    return meter;
+}
+result1 = kilometerToMeter(10);
+console.log(result1);
+    
+function budgetCalculator(watch,phone,laptop) {
+    var watchPrice  = watch * 50;
+    var phonePrice  = phone * 100;
+    var laptopPrice = laptop * 500;
+    var total       = (watchPrice + phonePrice + laptopPrice);
+
+    return total;
+}
+result2 = budgetCalculator(4,5,6);
+console.log(result2);
+
+function hotelCost(days){
+    
+}
+
+
+
+
+
